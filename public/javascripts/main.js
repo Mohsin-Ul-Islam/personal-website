8ddc630d08b2c6eae6713021a02f2feed213c719
Mohsin-Ul-Islam <mohsinulislam180@gmail.com>
2020-07-05 01:14:48 +0500

    added animations

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,30 @@
-window.addEventListener('load', function() {
-  setTimeout(function() {
-    document.querySelector('.preload').classList.remove('preload');
-    document.querySelector('.loader').style.display = 'none';
-  },0);
-});
+window.addEventListener('load', init);
+
+function init() {
+  document.querySelector('.preload').classList.remove('preload');
+  document.querySelector('.loader').style.display = 'none';
+  loop();
+}
+
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+      (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+
+function loop() {
+  let cards = document.querySelectorAll('.card');
+  cards.forEach((card) => {
+    if (isElementInViewport(card)) card.classList.add('float-up');
+  });
+  let animationDescriptor = window.requestAnimationFrame(loop);
+  if (Array.from(cards).every(card => card.classList.contains('float-up'))) window.cancelAnimationFrame(animationDescriptor);
+}
